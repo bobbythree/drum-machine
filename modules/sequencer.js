4d6c6f7949682b42003b5554fc234e54c96c4dc3
padesso <patrickadesso@gmail.com>
2023-12-09 08:56:54 -0500

    Play beats from array

--- modules/sequencer.js
+++ modules/sequencer.js
@@ -1,6 +1,13 @@
 //Constants
 const MAX_BEATS = 16;
 const BASS01_BEATS = [MAX_BEATS];
+const SNARE01_BEATS = [MAX_BEATS];
+const HIHAT01_BEATS = [MAX_BEATS];
+const RIDE_BEATS = [MAX_BEATS];
+const BASS02_BEATS = [MAX_BEATS];
+const SNARE02_BEATS = [MAX_BEATS];
+const HIHAT02_BEATS = [MAX_BEATS];
+const CRASH_BEATS = [MAX_BEATS];
 
 //Variables
 var isPlaying = false;
@@ -13,6 +20,18 @@ function Reset() {
     clearInterval(timer);
     timer = window.setInterval("Update()", TempoToMillis(tempo));  
     currentBeat = 0;
+
+    //Clear the stored beats
+    for(var i = 0; i < MAX_BEATS; i++) {
+        BASS01_BEATS[i] = 0; 
+        SNARE01_BEATS[i] = 0; 
+        HIHAT01_BEATS[i] = 0; 
+        RIDE_BEATS[i] = 0; 
+        BASS02_BEATS[i] = 0; 
+        SNARE02_BEATS[i] = 0; 
+        HIHAT02_BEATS[i] = 0; 
+        CRASH_BEATS[i] = 0; 
+    }
 }
 
 //Reset the sequencer
@@ -22,7 +41,9 @@ function Stop() {
         isPlaying = false;
         document.getElementById("playButton").style.backgroundColor="#a3a3a3";
 
-        Reset();
+        clearInterval(timer);
+        timer = window.setInterval("Update()", TempoToMillis(tempo));  
+        currentBeat = 0;
     }
 }
 
@@ -61,10 +82,34 @@ function Update() {
         }
         console.debug("Current beat: " + currentBeat);   
         
-        //TODO: Figure out which sound to play, for now HH
-        PlaySound(2);
-
-        
+        //Play sounds that are stored in the arrays as 1 (on) or 0 (off).  
+        //This could be improved to use a volume instead of o/1
+        for(var i = 0; i < MAX_BEATS; i++) {
+            if(BASS01_BEATS[i] == 1) {
+                PlaySound(0);
+            }
+            if(SNARE01_BEATS[i] == 1) {
+                PlaySound(1);
+            }
+            if(HIHAT01_BEATS[i] == 1) {
+                PlaySound(2);
+            }
+            if(RIDE_BEATS[i] == 1) {
+                PlaySound(3);
+            }
+            if(BASS02_BEATS[i] == 1) {
+                PlaySound(4);
+            }
+            if(SNARE02_BEATS[i] == 1) {
+                PlaySound(5);
+            }
+            if(HIHAT02_BEATS[i] == 1) {
+                PlaySound(6);
+            }
+            if(CRASH_BEATS[i] == 1) {
+                PlaySound(7);
+            }
+        }
     }
   }
 
